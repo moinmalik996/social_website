6aaba7dcbb8256d4b1746cb98cc6297b0298b052
Moin Abbas <moin.malik996@gmail.com>
2023-09-26 21:33:44 +0500

    django authentication and reset password implemented

--- account/urls.py
+++ account/urls.py
@@ -4,16 +4,19 @@ from . import views
 urlpatterns = [
     # path('login/', views.user_login, name='login')
     path('', views.home, name='home'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.UserLogoutVieew.as_view(), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('change-password/', views.UserPasswordChangeView.as_view(), name='change_password'),
-    path('change-password/done/', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
 
-    path('reset-password/', views.UserPasswordResetView.as_view(), name='password_reset'),
-    path('reset-password/done', views.UserPasswordResetView.as_view(), name='password_reset_done'),
-    path('reset-password/', views.UserPasswordResetView.as_view(), name='password_reset_confirm'),
-    path('reset-password/<uidb64>/<token>/', views.UserPasswordResetView.as_view(), name='password_reset_complete'),
+
+    
+    # path('login/', views.UserLoginView.as_view(), name='login'),
+    # path('logout/', views.UserLogoutVieew.as_view(), name='logout'),
+    # path('change-password/', views.UserPasswordChangeView.as_view(), name='change_password'),
+    # path('change-password/done/', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # path('reset-password/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    # path('reset-password/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset-password/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset-password/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 
