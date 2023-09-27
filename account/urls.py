5e9748f5dd710d96c9dcb317db769743ee5a778c
Moin Abbas <moin.malik996@gmail.com>
2023-09-27 11:34:41 +0500

    profile implemented

--- account/urls.py
+++ account/urls.py
@@ -7,16 +7,17 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
 
 
-    
-    # path('login/', views.UserLoginView.as_view(), name='login'),
-    # path('logout/', views.UserLogoutVieew.as_view(), name='logout'),
-    # path('change-password/', views.UserPasswordChangeView.as_view(), name='change_password'),
-    # path('change-password/done/', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    # path('reset-password/', views.UserPasswordResetView.as_view(), name='password_reset'),
-    # path('reset-password/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset-password/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset-password/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # Auth Views
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutVieew.as_view(), name='logout'),
+    path('change-password/', views.UserPasswordChangeView.as_view(), name='change_password'),
+    path('change-password/done/', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('reset-password/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('reset-password/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset-password/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset-password/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 
--- account/views.py
+++ account/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView,
                                        PasswordChangeDoneView, PasswordResetView,
                                        PasswordResetDoneView, PasswordResetConfirmView,
                                        PasswordResetCompleteView)
-from django.contrib.auth.decorators import login_required
 
-from .forms import UserForm
+from .models import Profile
+
+from .forms import UserForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
 def home(request):
     return render(request, 'accounts/home.html')
-
 
 def user_login(request):
     if request.method == 'POST':
@@ -42,6 +44,62 @@ def dashboard(request):
     data = dict()
     data['section'] = 'dashboard'
     return render(request, 'accounts/dashboard.html', data)
+
+def register(request):
+
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set the new password
+            new_user.set_password(user_form.cleaned_data['password'])
+
+            # Now Saving It To Database
+            new_user.save()
+
+            # creating User Profile
+            Profile.objects.create(user=new_user)
+            data = {
+                'new_user':new_user
+            }
+            return render(request, 'accounts/register_done.html', data)
+    else:
+        form = UserRegistrationForm()
+        data = {
+            'form':form
+        }
+    return render(request, 'accounts/register.html', data)
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+
+            return redirect('dashboard')
+
+        else:
+            messages.error(request, 'Error updating your profile.')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+
+    return render(
+        request,
+        'accounts/edit.html',
+        {'userr_form':user_form,
+         'profile_form': profile_form}
+    )
 
 class UserLoginView(LoginView):
     pass
